191fb37c268d417a8f937fafd0189167650ab2a1
Pramod Mishra_c <pramod.mishra_c@sandvik.com>
2020-10-21 17:34:19 +0530

    Added default seed for movie entity

---
+++ CArch.Infrastructure/Data/CArchApplicationDbContextSeed.cs
@@ -0,0 +1,31 @@
+﻿using CArch.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CArch.Infrastructure.Data
+{
+    public class CArchApplicationDbContextSeed
+    {
+        public static async Task SeedAsync(CArchApplicationDbContext cArchApplicationDbContext) {
+            await SeedMovieAsync(cArchApplicationDbContext);
+        }
+
+        private static async Task SeedMovieAsync(CArchApplicationDbContext cArchApplicationDbContext)
+        {
+            if (cArchApplicationDbContext.Movies.Any())
+                return;
+
+            var categories = new List<Movie>()
+            {
+                new Movie() { Name = "Batman"},
+              
+            };
+
+            cArchApplicationDbContext.Movies.AddRange(categories);
+            await cArchApplicationDbContext.SaveChangesAsync();
+        }
+    }
+}
--- CArch.WebAPI/Program.cs
+++ CArch.WebAPI/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using CArch.Infrastructure.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace CArch.WebAPI
@@ -14,9 +16,26 @@ namespace CArch.WebAPI
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            SeedDatabase(host);
+            host.Run();
         }
-
+        private static void SeedDatabase(IWebHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var cArchApplicationDbContext = services.GetRequiredService<CArchApplicationDbContext>();
+                    CArchApplicationDbContextSeed.SeedAsync(cArchApplicationDbContext).Wait();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
